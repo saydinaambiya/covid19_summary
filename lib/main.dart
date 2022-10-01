d8c6eab4e3d2aa9ab094912f5ba490d297fcf332
saydinaambiya <ssyyddn@gmail.com>
2022-10-01 23:35:26 +0700

    build app

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: OnboardingPage(),
+    );
+  }
+}
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQueryHeight = MediaQuery.of(context).size.height;
+    final mediaQueryWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Container(
+        height: mediaQueryHeight * 0.7,
+        width: mediaQueryWidth,
+        child: Image.asset(
+          'assets/images/illustration.png',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
